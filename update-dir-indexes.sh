066b89a45e2d8f8be669eb7ef8e3cd1d75fe141b
Gus Dunn <wadunn83@gmail.com>
2016-02-23 17:01:23 -0500

    added noarch to updateindex script

--- update-dir-indexes.sh
+++ update-dir-indexes.sh
@@ -4,13 +4,13 @@ update_dir_indexes () {
     for DIR in $(find ./pkgs/channel/$1 -type d); do
       (
         echo -e "<html>\n<body>\n<h1>Available packages</h1>\n<hr/>\n<pre>"
-        ls -1pa "${DIR}" | grep .tar.bz2 | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }' 
+        ls -1pa "${DIR}" | grep .tar.bz2 | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
         echo -e "</pre>\n</body>\n</html>"
       ) > "${DIR}/index.html"
     done
 }
 
-
+update_dir_indexes 'noarch'
 update_dir_indexes 'linux-32'
 update_dir_indexes 'linux-64'
 update_dir_indexes 'osx-64'
